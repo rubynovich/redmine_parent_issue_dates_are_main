6b87b9ce92b7d0ace06b234dcfe43e01e5cd1891
Rubynovich <roman@shipiev.me>
2012-05-22 14:56:04 +0400

    first commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_parent_issue_dates_are_main do
+  name 'Redmine Parent Issue Dates Are Main plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
