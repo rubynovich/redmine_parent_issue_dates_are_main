403eb471e2f416b629fd96894f79b61fdab92bb0
Pereslavtseva <e.pereslavtseva@gorkapstroy.ru>
2014-08-08 11:36:47 +0400

    Fix: изменен текст письма-4

--- lib/pidam_mailer_patch.rb
+++ lib/pidam_mailer_patch.rb
@@ -19,10 +19,10 @@ module ParentIssueDatesAreMainPlugin
           @issue_title = "##{@issue.id} \"#{@issue.subject}\""
           @old_p = priorities["old_p"]
           @new_p = priorities["new_p"]
-          @author = User.where(id: @issue.author_id).name
-          @status = IssueStatus.where(id: @issue.status_id).name
-          @priority = IssuePriority.where(id: @issue.priority_id).name
-          @assigned_to = User.where(id: @issue.assigned_to_id).name
+          @author = User.where(id: @issue.author_id).first.name
+          @status = IssueStatus.where(id: @issue.status_id).first.name
+          @priority = IssuePriority.where(id: @issue.priority_id).first.name
+          @assigned_to = User.where(id: @issue.assigned_to_id).first.name
           mail :to => user.mail, 
                :subject => l(:subject_parent_priority_was_changed, project_and_issue: "#{@issue.project.name} - ##{@issue.id}", iss_subject: @issue.subject)
         end
